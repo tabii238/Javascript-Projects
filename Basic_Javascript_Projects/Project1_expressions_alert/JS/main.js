725ba1d8efc800e9d61dab42d077dd3da627bd07
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-06-10 22:49:59 -0700

    Add files via upload

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello World!"); // display text in window //
+
+document.write("Hello World!"); // utilizing document.write method to display text //
+
+var A= "Today is June 10th"; // assigning string value to variable //
+var A = A.fontcolor("blue"); // using the fontcolor method on variable //
+document.write(A); // printing text in blue //
+
+document.write("\"The show will be packed,\" she warned them."); // escaping the character //
+
+document.write("Take a right at the four-way stop," // concatenating a string //
++" continue straight 2 miles,"
++" and turn left into the parking garage at the corner.");
+
+var Y = "It is $2 to park for an hour," + " and $20 to park all day."; // assigning a concatenating string to a variable //
+var Y =Y.fontcolor("red");
+document.write(Y) // printing the variable //
+
+
+var Band = "Big Thief", Vocals = "Adrianne", guitar = "Buck", bass = "Max", drum ="James"; // assigning multiple variables //
+var Band = Band.fontcolor("green");
+document.write(Band); // printing one //
+
+
+document.write(20/12); // an expression //
+
+
---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/string_assignment.js
@@ -0,0 +1,3 @@
+var X="Tomorrow is Friday";
+
+window.alert(X);
