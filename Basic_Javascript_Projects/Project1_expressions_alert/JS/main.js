bbf887e29880ee068337a19e4f5d225d4e37d3fa
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-06-12 12:02:06 -0700

    corrected concatenation portion

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,27 +1,29 @@
-window.alert("Hello World!"); // display text in window //
+window.alert("Hello World!"); // display text in window 
 
-document.write("Hello World!"); // utilizing document.write method to display text //
+document.write("Hello World!"); // utilizing document.write method to display text 
 
-var A= "Today is June 10th"; // assigning string value to variable //
-var A = A.fontcolor("blue"); // using the fontcolor method on variable //
-document.write(A); // printing text in blue //
+var A= "Today is June 10th"; // assigning string value to variable 
+var A = A.fontcolor("blue"); // using the fontcolor method on variable 
+document.write(A); // printing text in blue 
 
-document.write("\"The show will be packed,\" she warned them."); // escaping the character //
+document.write("\"The show will be packed,\" she warned them."); // escaping the character 
 
-document.write("Take a right at the four-way stop," // concatenating a string //
+document.write("Take a right at the four-way stop," // concatenating a string 
 +" continue straight 2 miles,"
 +" and turn left into the parking garage at the corner.");
 
-var Y = "It is $2 to park for an hour," + " and $20 to park all day."; // assigning a concatenating string to a variable //
-var Y =Y.fontcolor("red");
-document.write(Y) // printing the variable //
+var str1= "It is $2 to park for an hour."; // assigning a string value to a variable 
+var str1=str1.fontcolor("red"); // assigning fontcolor
+var str2= "It is $20 to park all day."; // assigning a string value to a separate variable 
+var res=str1.concat(str2); // concatenating the variables
+document.write(res); // displaying both strings
 
 
-var Band = "Big Thief", Vocals = "Adrianne", guitar = "Buck", bass = "Max", drum ="James"; // assigning multiple variables //
+var Band = "Big Thief", Vocals = "Adrianne", guitar = "Buck", bass = "Max", drum ="James"; // assigning multiple variables 
 var Band = Band.fontcolor("green");
-document.write(Band); // printing one //
+document.write(Band); // printing one 
 
 
-document.write(20/12); // an expression //
+document.write(20/12); // an expression 
 
 
