07676d438c445970a933717ab608c5e8125452bd
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-06-12 22:32:17 -0700

    pushing math operations folder to repo

---
+++ Basic_Javascript_Projects/Project3_math_operations/JS/main.js
@@ -0,0 +1,38 @@
+function myFunction(a, b) { // addition function
+    return a + b;
+}
+
+function subtractFunction(x, y) { // subtraction function
+    return x - y;
+}
+
+function multiplyFunction(c, d) { // multiplication function
+    return c * d;
+}
+
+function divideFunction(e, f) { // division function
+    return e / f;
+}
+
+function operationsFunction(g, h, i, j, k) { // multiple operations function
+    return g + h * i / j - k;
+}
+
+function modulusFunction(l, m) { // modulus function (remainder)
+    return l % m;
+}
+
+function negationFunction(n) { // negation function
+    return -n;
+}
+
+var o = 7; // increment operation
+o++;
+document.write("The result of the increment operation when applied to the number 7 = " + o);
+
+var p = 10; // decrement operation
+p--;
+document.write(". The result of the decrement operation when applied to the number 10 = " + p);
+
+window.alert(Math.random() * 50); // random number window alert
+
