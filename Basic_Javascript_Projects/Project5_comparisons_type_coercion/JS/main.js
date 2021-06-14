2d190b61f6cad36ca00281d84c3b5c3a442801db
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-06-14 01:07:03 -0700

    assignment files

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,47 @@
+document.write(typeof "book"); 
+
+document.write(" Saturday" + 5);
+
+document.write(4e332);
+
+document.write(-2e310);
+
+document.write(9 > 2);
+
+document.write(4 < 0);
+
+console.log(15*3);
+
+console.log(5>15);
+
+document.write(9 == 10);
+
+x = 12;
+y = 12;
+document.write(x === y);
+
+c = "red";
+d = 3;
+document.write(c === d);
+
+a = "12";
+b = 12;
+document.write(a === b);
+
+document.write(12 === 10);
+
+document.write(7 > 2 && 1 > 8);
+
+document.write(4 > 3 && 10 > 8); 
+
+document.write(25 > 10 || 34 < 40);
+
+document.write(17 > 23 || 2 > 8 );
+
+function not_Function() {
+    document.getElementById("not_one").innerHTML = !(3 > 4);
+}
+
+function not_two_Function() {
+    document.getElementById("not_two").innerHTML =!(5 > 2);
+}
