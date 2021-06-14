ebb931e5b02c813490f4a30ff13f16fc389e9744
Tabitha Y <63029748+tabii238@users.noreply.github.com>
2021-06-14 00:07:11 -0700

    corrections

--- Basic_Javascript_Projects/Project3_math_operations/JS/main.js
+++ Basic_Javascript_Projects/Project3_math_operations/JS/main.js
@@ -1,33 +1,41 @@
-function myFunction(a, b) { // addition function
-    return a + b;
+function myFunction(a, b) {
+    var sum=(a + b); // addition function
+    document.getElementById("add").innerHTML=sum;
 }
 
 function subtractFunction(x, y) { // subtraction function
-    return x - y;
+    var difference=(x - y);
+    document.getElementById("subtract").innerHTML=difference;
 }
 
 function multiplyFunction(c, d) { // multiplication function
-    return c * d;
+    var product=(c * d);
+    document.getElementById("multiply").innerHTML=product;
 }
 
 function divideFunction(e, f) { // division function
-    return e / f;
+    var quotient=(e / f);
+    document.getElementById("divide").innerHTML=quotient;
 }
 
 function operationsFunction(g, h, i, j, k) { // multiple operations function
-    return g + h * i / j - k;
+    var result=(g + h * i / j - k);
+    document.getElementById("multiple_operations").innerHTML=result;
 }
 
 function modulusFunction(l, m) { // modulus function (remainder)
-    return l % m;
+    var remainder=(l % m);
+    document.getElementById("modulus").innerHTML=remainder;
 }
 
 function negationFunction(n) { // negation function
-    return -n;
+    var negation=(-n);
+    document.getElementById("negation").innerHTML=negation;
+
 }
 
 var o = 7; // increment operation
-o++;
+o++;    
 document.write("The result of the increment operation when applied to the number 7 = " + o);
 
 var p = 10; // decrement operation
@@ -35,4 +43,3 @@ p--;
 document.write(". The result of the decrement operation when applied to the number 10 = " + p);
 
 window.alert(Math.random() * 50); // random number window alert
-
